907a3ae2350e4a71ca8d7cf6121fa27c7612fb1f
Rory Butler <rb2661@columbia.edu>
2023-03-03 16:52:14 -0500

    cleaned up code

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from dash import Dash, dcc, Input, Output, dash_table
 from dash import html as html
 
 app = Dash(__name__)
-app.title = "NYC High Schools"
+app.title = "NYC High School Demographics"
 
 # data loading
 schools = pd.read_csv('data/schools_final.csv')
@@ -18,7 +18,8 @@ app.layout = html.Div([
 
     html.Div([
         html.H1('NYC High School Demographics'),
-        html.P('Select a demographic and year:'),
+        html.P('Select a demographic and year to update the map of NYC'),
+        html.P('Click on a zip code to see data specific to schools in that zip code'),
         dcc.Dropdown(
             id='demographic',
             options=['Graduation Rate', 'Asian %', 'Hispanic %', 'Black %', 'Poverty %'],
@@ -30,20 +31,23 @@ app.layout = html.Div([
                 value=2017,
                 inline=True
             )
-    ], style={'width': '60%', 'display': 'inline-block'}),
+    ], style={'width': '60%', 'display': 'inline-block', 'font-family': 'Helvetica'}),
 
-    html.Div([
-        dcc.Graph(id='map')
-    ],  style={'width': '60%', 'display': 'inline-block', 'padding': '0 20'}),
-    html.Div([
-        dcc.Graph(id='time-series')
-    ], style={'display': 'inline-block', 'float': 'right', 'width': '40%'}),
+    html.Div(
+        dcc.Loading(id='loading-graph',
+                    children=[html.Div([dcc.Graph(id='map')])],
+                    type='default'
+                    ),
+        style={'width': '60%', 'display': 'inline-block', 'padding': '0 10'}),
     html.Div([
         html.H2('Selected Zip Code:', style={'textAlign': 'center'}),
         html.H4(id='zip_code', style={'textAlign': 'center'}),
+        dcc.Graph(id='time-series'),
         html.H4(id='schools_table')
-    ], style={'width': '40%'})
+    ], style={'display': 'inline-block', 'float': 'right', 'width': '40%', 'font-family': 'Helvetica'}),
+
 ])
+
 
 @app.callback(
     Output('map', 'figure'),
@@ -53,10 +57,10 @@ def display_choropleth(demographic, year):
     df = zip_means[zip_means['Cohort Year'] == year]
     geojson = zip_geos
     fig = px.choropleth_mapbox(df, geojson=geojson, color=demographic,
-                           locations='Zipcode', featureidkey='properties.ZIPCODE',
-                           center={'lat': 40.7128, 'lon': -74.0060},
-                           mapbox_style='carto-positron', zoom=9)
-    fig.update_layout(margin={'r':0,'t':0,'l':0,'b':0})
+                               locations='Zipcode', featureidkey='properties.ZIPCODE',
+                               center={'lat': 40.7128, 'lon': -74.0060},
+                               mapbox_style='carto-positron', zoom=9)
+    fig.update_layout(margin={'r': 0, 't': 0, 'l': 0, 'b': 0}, height=700)
 
     # add school points
     '''
@@ -69,12 +73,14 @@ def display_choropleth(demographic, year):
     '''
     return fig
 
+
 @app.callback(
     Output('zip_code', 'children'),
     Input('map', 'clickData'))
 def display_zip_code(clickData):
     zip_display = clickData['points'][0]['location']
     return zip_display
+
 
 @app.callback(
     Output('schools_table', 'children'),
@@ -91,6 +97,7 @@ def display_school_table(clickData, demographic, year):
     schooltable = dash_table.DataTable(
         data=df_data,
         columns=columns,
+        sort_action='native',
         style_table={'overflowX': 'auto'},
         style_header={
             'fontWeight': 'bold',
@@ -118,6 +125,7 @@ def display_school_table(clickData, demographic, year):
 
     return schooltable
 
+
 @app.callback(
     Output('time-series', 'figure'),
     Input('map', 'clickData'),
@@ -128,8 +136,8 @@ def time_series_plot(clickData, demographic):
     schools4 = schools3.sort_values(by=['School Name', 'Cohort Year'], ascending=[True, True])
 #    schools4.dropna(inplace=True)
     fig = px.line(schools4, x='Cohort Year', y=demographic, color='School Name',
-                  title='School Demographics by Year', markers=True)
-    fig.update_layout(showlegend=False)
+                  title='School Demographic by Year', markers=True)
+    fig.update_layout(showlegend=False, title_x=0.5)
     fig.update_xaxes(nticks=5)
     fig.add_annotation(x=0, y=0.85, xanchor='left', yanchor='bottom',
                        xref='paper', yref='paper', showarrow=False, align='left', text='')
@@ -137,11 +145,6 @@ def time_series_plot(clickData, demographic):
     fig.update_layout(height=500, margin={'l': 10, 'b': 10, 'r': 10, 't': 40})
 
     return fig
-# hover barchart
-'''
-def display_hover(hoverData):
-    schools2 = schools[schools['postcode'] == hoverData['points'][0]['location']]
-    fig = px.bar(schools2, x='school_name', y='Total Grads % of cohort')
-    return fig
-'''
+
+
 app.run_server(debug=True)
--- nyc_schools.py
+++ nyc_schools.py
@@ -1,22 +1,13 @@
 import pandas as pd
 import geopandas as gpd
-import plotly.express as px
-import folium
-from folium.plugins import MarkerCluster
-import webbrowser
-import numpy as np
-import matplotlib.pyplot as plt
-from dash import Dash, html, dcc
 
+# dataset containing all high schools in NYC
 schools = pd.read_csv('data/2021_DOE_High_School_Directory.csv')
 
-# columns we're interested in:
-# dbn, school_name, Borough, postcode, latitude, longitude, url, total_students, graduation_rate, attendance_rate, NTA, postcode
+schools = schools[['dbn', 'school_name', 'Borough', 'Latitude', 'Longitude', 'url', 'total_students', 'graduation_rate',
+                   'attendance_rate', 'NTA', 'postcode']]
 
-schools = schools[['dbn', 'school_name', 'Borough', 'Latitude', 'Longitude', 'url', 'total_students', 'graduation_rate', 'attendance_rate', 'NTA', 'postcode']]
-
-
-# update Gotham DBN so it joins to the other 2 data sets
+# update Gotham DBN, so it joins to the other 2 data sets
 schools.loc[schools['school_name'] == 'Gotham Professional Arts Academy', 'dbn'] = '16K594'
 
 # update arts academy zip code
@@ -25,6 +16,7 @@ schools.loc[schools['school_name'] == 'Art and Design High School', 'postcode'] 
 # grad rates data -- much more complete than the original data
 schools_grad = pd.read_csv('data/2016-2017_Graduation_Outcomes_School.csv')
 
+# filtering to only graduation statistics for 4-year graduation rates for all students
 cohort_mask = schools_grad['Cohort'] == '4 year June'
 year_mask = schools_grad['Cohort Year'] >= 2009
 type_mask = schools_grad['Demographic Category'] == 'All Students'
@@ -32,16 +24,11 @@ type_mask = schools_grad['Demographic Category'] == 'All Students'
 schools_grad2 = schools_grad[cohort_mask][year_mask][type_mask]
 
 # join
-
 schools_full = schools.merge(schools_grad2, left_on='dbn', right_on='DBN')
 
 schools_demo = pd.read_csv('data/2013_-_2018_Demographic_Snapshot_School.csv')
 
-#year_mask2 = schools_demo['Year'] == '2016-17'
-
-#schools_demo2 = schools_demo[year_mask2]
-
-#changing year format to be joinable
+# changing year format to be joinable
 schools_demo['Year'] = schools_demo['Year'].str[:-3]
 
 schools_demo['Year'] = schools_demo['Year'].apply(pd.to_numeric)
@@ -49,14 +36,6 @@ schools_demo['Year'] = schools_demo['Year'].apply(pd.to_numeric)
 schools_full['Cohort Year'] = schools_full['Cohort Year'] + 4
 
 schools_full2 = schools_full.merge(schools_demo, left_on=['dbn', 'Cohort Year'], right_on=['DBN', 'Year'])
-
-fig = px.scatter(schools_full2, x='Total Grads % of cohort', y='% Poverty', size='Total Cohort #', color='Borough')
-
-#fig.show()
-
-fig2 = px.histogram(schools_full2, x='Total Cohort #', y='% Asian', histfunc='avg', nbins=20)
-
-#fig2.show()
 
 schools_full2['postcode'] = schools_full2['postcode'].apply(lambda x: str(x).replace(",", ""))
 
@@ -66,53 +45,34 @@ schools_final = schools_full2[['dbn', 'school_name', 'Borough', 'Latitude', 'Lon
                                '# Asian', '% Asian', '# Black', '% Black', '# Hispanic', '% Hispanic', '# Poverty',
                                '% Poverty']]
 
-# zip codes geojson
+# importing geojson containing nyc zip code geographic data
 
 nyc_zip = gpd.read_file('data/zip_code_040114.geojson')
 nyc_zip = nyc_zip[['ZIPCODE', 'geometry']]
 
 zips = nyc_zip['ZIPCODE'].value_counts()
 
+# Some zip code polygons have duplicate rows. I kept the first row for each that appeared.
 nyc_zip.drop([109, 113, 114, 146, 149, 259, 27, 28, 123, 246, 19, 253, 241, 194, 144], inplace=True)
-
-"""I have no idea which zip code polygons are correct for the duplicate rows. I'm just going to keep the first of each and can modify if they look wrong.
-
-10004: Keep 106, drop 109, 113, 114
-
-11693: Keep 134, drop 146, 149, 259
-
-10464: Keep 16, drop 27, 28
-
-11231: Keep 118, drop 123
-
-10047: Keep 245, drop 246
-
-10463: Keep 14, drop 19
-
-10035: Keep 49, drop 253
-
-10196: Keep 240, drop 241
-
-11370: Keep 65, drop 194
-
-11096: Keep 139, drop 144
-
-"""
 
 nyc_geo = nyc_zip.merge(schools_final, how='outer', left_on='ZIPCODE', right_on='postcode')
 
-nyc_geo.ZIPCODE.value_counts().plot(kind='barh', figsize=(20,16))
+# nyc_geo.ZIPCODE.value_counts().plot(kind='barh', figsize=(20,16))
 
-mean_values = nyc_geo[['ZIPCODE', 'Cohort Year', 'Total Cohort #', 'Total Grads #', 'Total Grads % of cohort', '# Female', '% Female',
-       '# Male', '% Male', '# Asian', '% Asian', '# Black', '% Black', '# Hispanic', '% Hispanic', '# Poverty', '% Poverty']].groupby(['ZIPCODE', 'Cohort Year']).mean()
+# creating dataframe containing mean values for schools in the same zip code
+# this aggregate data will allow for choropleth mapping
+mean_values = nyc_geo[['ZIPCODE', 'Cohort Year', 'Total Cohort #', 'Total Grads #', 'Total Grads % of cohort',
+                       '# Female', '% Female', '# Male', '% Male', '# Asian', '% Asian', '# Black', '% Black',
+                       '# Hispanic', '% Hispanic', '# Poverty', '% Poverty']].groupby(['ZIPCODE', 'Cohort Year']).mean()
 
-cols = ['mean cohort #', 'mean grad #', 'mean grad %', 'mean female #', 'mean female %', 'mean male #', 'mean male %', 'mean asian #', 'mean asian %',
-        'mean black #', 'mean black %', 'mean hispanic #', 'mean hispanic %', 'mean poverty #', 'mean poverty %']
+cols = ['mean cohort #', 'mean grad #', 'mean grad %', 'mean female #', 'mean female %', 'mean male #', 'mean male %',
+        'mean asian #', 'mean asian %', 'mean black #', 'mean black %', 'mean hispanic #', 'mean hispanic %',
+        'mean poverty #', 'mean poverty %']
 
 mean_values.columns = cols
 mean_values = mean_values.reset_index()
 
-zip_means = nyc_geo[['ZIPCODE', 'geometry']].merge(mean_values, left_on = 'ZIPCODE', right_on = 'ZIPCODE')
+zip_means = nyc_geo[['ZIPCODE', 'geometry']].merge(mean_values, left_on='ZIPCODE', right_on='ZIPCODE')
 
 zip_means.drop_duplicates(inplace=True)
 
@@ -120,105 +80,7 @@ zip_means_gdf = gpd.GeoDataFrame(zip_means, crs='EPSG:4326', geometry=zip_means.
 
 zip_geos = zip_means_gdf[['ZIPCODE', 'geometry']]
 
-def popup_html(row):
-    i = row
-    school_name = schools_final['school_name'].iloc[i]
-    school_size = schools_final['total_students'].iloc[i]
-    grad_cohort = schools_final['Total Grads #'].iloc[i]
-    grad_pct = schools_final['Total Grads % of cohort'].iloc[i]
-    gender = schools_final['% Female'].iloc[i]
-    asian = schools_final['% Asian'].iloc[i]
-    black = schools_final['% Black'].iloc[i]
-    hispanic = schools_final['% Hispanic'].iloc[i]
-    url = schools_final['url'].iloc[i]
-
-    left_col_color = "#73A5C6"
-    right_col_color = "#f2f9ff"
-    
-    html = """
-<!DOCTYPE html>
-<html>
-<center><h4 style="margin-bottom:5"; width="200px">{}</h4>""".format(school_name) + """</center>
-<center><a href=\"""" + url + """\">View School Profile</a></center>
-<center> <table style="height: 126px; width: 225px;">
-<tbody>
-<tr>
-<td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;"> Current School Size (2021) </span></td>
-<td style="width: 50px;background-color: """+ right_col_color +""";"><center>"""+school_size +"""</td></center>
-</tr>
-<tr>
-<td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;"> Graduating Class Size (2017) </span></td>
-<td style="width: 50px;background-color: """+ right_col_color +""";"><center>{}</td></center>""".format(grad_cohort) + """
-</tr>
-<tr>
-<td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;"> Graduation Rate (2017) (%) </span></td>
-<td style="width: 50px;background-color: """+ right_col_color +""";"><center>{}</td></center>""".format(grad_pct) + """
-</tr>
-<tr>
-<td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;"> Female (%) </span></td>
-<td style="width: 50px;background-color: """+ right_col_color +""";"><center>{}</td></center>""".format(gender) + """
-</tr>
-<tr>
-<td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;"> Asian (%)</span></td>
-<td style="width: 50px;background-color: """+ right_col_color +""";"><center>{}</td></center>""".format(asian) + """
-</tr>
-<tr>
-<td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;"> Black (%)</span></td>
-<td style="width: 50px;background-color: """+ right_col_color +""";"><center>{}</td></center>""".format(black) + """
-</tr>
-<tr>
-<td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;"> Hispanic (%)</span></td>
-<td style="width: 50px;background-color: """+ right_col_color +""";"><center>{}</td></center>""".format(hispanic) + """
-</tr>
-</tbody>
-</table></center>
-</html>
-"""
-    return html
-
-# new choropleth map
-coords = [40.7128, -74.0060]
-
-nyc_zip_map = folium.Map(coords, tiles='OpenStreetMap', zoom_start = 12)
-
-folium.Choropleth(
-    geo_data = zip_geos,
-    name = 'Zip Codes',
-    data = zip_means,
-    columns = ['ZIPCODE', 'mean grad #'],
-    key_on = 'feature.id',
-    fill_color = 'YlGnBu',
-    fill_opacity = 0.5,
-    line_opacity = 1,
-    legend_name = 'Mean Graduating Class Size',
-    smooth_factor = 0
-).add_to(nyc_zip_map)
-
-#fg = folium.FeatureGroup('School Locations').add_to(nyc_zip_map)
-
-marker_cluster = MarkerCluster(name='School Locations').add_to(nyc_zip_map)
-
-for i in range(0,len(schools_final)):
-
-  #popup 
-  html = popup_html(i)
-  popup = folium.Popup(folium.Html(html, script=True), max_width=500)
-
-  marker_cluster.add_child(folium.Marker(
-      location=[schools_final.iloc[i]['Latitude'], schools_final.iloc[i]['Longitude']],
-      popup=popup,
-      icon=folium.Icon(icon='mortar-board', prefix='fa')
-  )).add_to(nyc_zip_map)
-
-folium.LayerControl().add_to(nyc_zip_map)
-
-#nyc_zip_map.save("figures/nyc_zip_map.html")
-#webbrowser.open("figures/nyc_zip_map.html")
-
 # save dataframes into csv files for use in app.py
-
-#print(zip_means.columns)
-
 zip_new_cols = ['Zipcode', 'geometry', 'Cohort Year', 'Graduating Cohort', 'Graduates', 'Graduation Rate',
                 'Female Student Count', 'Female %', 'Male Student Count', 'Male %',
                 'Asian Student Count', 'Asian %', 'Black Student Count', 'Black %',
@@ -226,14 +88,12 @@ zip_new_cols = ['Zipcode', 'geometry', 'Cohort Year', 'Graduating Cohort', 'Grad
 
 zip_means.columns = zip_new_cols
 
-#print(schools_final.columns)
-
 schools_new_cols = ['dbn', 'School Name', 'Borough', 'Latitude', 'Longitude', 'url',
                     'Total Students', '2021 Grad Rate', '2021 Attendance Rate', 'NTA',
                     'Zipcode', 'Cohort Year', 'Graduating Cohort', 'Graduates', 'Graduation Rate',
-                'Female Student Count', 'Female %', 'Male Student Count', 'Male %',
-                'Asian Student Count', 'Asian %', 'Black Student Count', 'Black %',
-                'Hispanic Student Count', 'Hispanic %', 'Students in Poverty Count', 'Poverty %']
+                    'Female Student Count', 'Female %', 'Male Student Count', 'Male %',
+                    'Asian Student Count', 'Asian %', 'Black Student Count', 'Black %',
+                    'Hispanic Student Count', 'Hispanic %', 'Students in Poverty Count', 'Poverty %']
 
 schools_final.columns = schools_new_cols
 
@@ -242,10 +102,7 @@ schools_final.to_csv('data/schools_final.csv')
 
 zip_geos.to_file('data/zip_geos.json', driver='GeoJSON')
 
-# testing px.choropleth
-
-
 # Art and Design High School -- wrong postal code: change from 10019 to 10022
 # Eagle Academy for Young Men of Staten Island, The - no data in grad rate dataset
-# Gotham Professional Arts Academy - wrong DBN number in original dataset (change from 13K594 to 16K594 in original dataset so that it gets joined)
+# Gotham Professional Arts Academy - (change dbn from 13K594 to 16K594 in original dataset so that it gets joined)
 # Epic High School - South - no data in grad rate dataset
